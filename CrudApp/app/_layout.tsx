40192298e8187c0e11fb61424fb23abedf34b488
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-01-14 22:56:40 -0300

    Feat: Implemented the application's header. Feat: Started the FlatList Implementation.

--- CrudApp/app/_layout.tsx
+++ CrudApp/app/_layout.tsx
@@ -1,5 +1,43 @@
 import { Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+
+type GradientHeaderProps = {
+  title: string;
+};
+
+function GradientHeader({ title }: GradientHeaderProps) {
+  return (
+    <LinearGradient
+      colors={["#9B87F5", "#7E69AB"]}
+      start={{x: 0, y: 0}}
+      end={{x: 1, y: 1}}
+      style={styles.headerContainer}
+    >
+      <Text style={styles.headerText}>{title}</Text>
+    </LinearGradient>
+  );
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ 
+    header: () => <GradientHeader title="My Tasks"/>
+  }} />;
 }
+
+
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 80, // Adjust height as per your needs
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+  },
+})
--- CrudApp/app/index.tsx
+++ CrudApp/app/index.tsx
@@ -1,19 +1,57 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import data from '../data/data.json'
+import Octicons from '@expo/vector-icons/Octicons';
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <FlatList 
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listContainer}
+        renderItem={({item}) => (
+          <View style={styles.taskContainer}>
+            {item.completed 
+              ? <Octicons name="check-circle-fill" size={27} color="#9B87F5" style={{ marginRight: 15 }} /> 
+              : <Octicons name="check-circle" size={27} color="#a3a3a4" style={{ marginRight: 15 }} />
+            }
+            <Text style={{... styles.taskText, textDecorationLine: item.completed ? 'line-through' : 'none'}}>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    paddingVertical: 20,
+    paddingHorizontal: 23,
+    width: '100%',
+  },
+  listContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  taskContainer: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#FFF',
+    marginBottom: 18,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#d1d0d0a0',
+    paddingHorizontal: 15,
+    overflow: 'hidden',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  taskText: {
+    fontSize: 16,
+    color: '#3a3a3a',
+    textDecorationColor: '#3a3a3a'
+  },
 });
