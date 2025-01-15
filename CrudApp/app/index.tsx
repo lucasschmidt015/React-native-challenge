7db3dc8bc8b3d0dd4864411347cd3147acd14e8d
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-01-14 21:03:23 -0300

    Starting the project.

---
+++ CrudApp/app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
