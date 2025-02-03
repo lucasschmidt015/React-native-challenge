e53bcab96091b434a655a5fe12b862b83552e811
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-02-03 20:39:48 -0300

    Setting jest up to tests.

---
+++ API/src/test/taskController.spec.ts
@@ -0,0 +1,10 @@
+import { getTasks } from "../controllers/task";
+
+
+describe('These functions should retrive tasks data', () => {
+
+    it('Should return all taks in the database', async () => {
+
+    });
+
+});
